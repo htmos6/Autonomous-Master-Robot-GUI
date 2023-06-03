20ed051487972feaf859b9fe54e6f7a13f5bdbac
htmos6 <mehmet.arslan4000@gmail.com>
2023-06-03 16:15:19 +0300

    UI path selection functions are added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,6 +15,12 @@ public:
     void printPoints();
     void connect();
     void drawPath();
+    void sel1();
+    void sel2();
+    void sel3();
+    void sel4();
+    void sel5();
+    void sel6();
 
 private slots:
     void on_actionPrint_triggered();
@@ -63,9 +69,34 @@ void MainWindowPrivate::connect()
     m_drawWidget->connectToPico();
 }
 
-void MainWindowPrivate::drawPath()
+void MainWindowPrivate::sel1()
 {
-    m_drawWidget->drawPath();
+    m_drawWidget->drawPath(1);
+}
+
+void MainWindowPrivate::sel2()
+{
+    m_drawWidget->drawPath(2);
+}
+
+void MainWindowPrivate::sel3()
+{
+    m_drawWidget->drawPath(3);
+}
+
+void MainWindowPrivate::sel4()
+{
+    m_drawWidget->drawPath(4);
+}
+
+void MainWindowPrivate::sel5()
+{
+    m_drawWidget->drawPath(5);
+}
+
+void MainWindowPrivate::sel6()
+{
+    m_drawWidget->drawPath(6);
 }
 
 MainWindowPrivate::~MainWindowPrivate(){
@@ -102,9 +133,34 @@ void MainWindow::on_actionSend_triggered()
     d->printPoints();
 }
 
-void MainWindow::drawPath()
+void MainWindow::sel1()
 {
-    d->drawPath();
+    d->sel1();
+}
+
+void MainWindow::sel2()
+{
+    d->sel2();
+}
+
+void MainWindow::sel3()
+{
+    d->sel3();
+}
+
+void MainWindow::sel4()
+{
+    d->sel4();
+}
+
+void MainWindow::sel5()
+{
+    d->sel5();
+}
+
+void MainWindow::sel6()
+{
+    d->sel6();
 }
 
 void MainWindow::clear()
--- mainwindow.h
+++ mainwindow.h
@@ -33,7 +33,12 @@ public slots:
     void clear();
     void printPoints();
     void on_actionSend_triggered();
-    void drawPath();
+    void sel1();
+    void sel2();
+    void sel3();
+    void sel4();
+    void sel5();
+    void sel6();
     void on_actionConnect_triggered();
 
 private:
